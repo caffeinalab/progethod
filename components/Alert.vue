8d7308e43eafa9fe3b633d370481f81110d6cf43
Stefano Fasoli <stefanofasoli17@gmail.com>
2026-07-01 14:25:18 +0200

    feat: dismissable month-end reminder banner

--- components/Alert.vue
+++ components/Alert.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex items-center justify-center px-4 sm:px-0">
-    <div id="alert" class="lg:w-10/12 transition duration-150 ease-in-out shadow rounded-md md:flex justify-between items-center top-0 mt-4 mb-4 py-4 px-4" :class="bgColor">
+  <div class="container px-6 mx-auto">
+    <div id="alert" class="transition duration-150 ease-in-out shadow rounded-md md:flex justify-between items-center mt-4 mb-4 py-4 px-4" :class="bgColor">
       <div class="sm:flex items-center">
         <div class="flex items-end">
           <alert-circle-icon class="mr-2 mt-0.5 sm:mt-0" :class="iconColor" />
@@ -11,19 +11,26 @@
         <div class="h-1 w-1 rounded-full mr-2 hidden xl:block" :class="iconColor" />
         <p class="text-base" :class="textColor" v-html="message" />
       </div>
+      <button
+        v-if="dismissable"
+        class="ml-auto pl-4 flex-shrink-0 transition-colors"
+        :class="textColor + ' hover:opacity-70'"
+        @click="$emit('dismiss')"
+      >
+        <x-icon size="18" />
+      </button>
     </div>
   </div>
 </template>
 
 <script>
-import {
-  AlertCircleIcon
-} from 'vue-tabler-icons'
+import { AlertCircleIcon, XIcon } from 'vue-tabler-icons'
 
 export default {
   name: 'ColorCodedWithIconWarning',
   components: {
-    AlertCircleIcon
+    AlertCircleIcon,
+    XIcon
   },
   props: {
     message: {
@@ -33,6 +40,10 @@ export default {
     level: {
       type: String,
       default: 'warning'
+    },
+    dismissable: {
+      type: Boolean,
+      default: false
     }
   },
   computed: {
